2ad8210f51782f23716bfba33fcead69485d57c1
nkumar007 <naveen.mbuff98@gmail.com>
2023-08-19 16:01:18 +0530

    setting up our services

---
+++ app.js
@@ -0,0 +1,3 @@
+// It's better to check for this event in app.js, because it's the first thing that loads in the browser.
+
+window.addEventListener("DOMContentLoaded", () => {});
---
+++ services/API.js
@@ -0,0 +1,9 @@
+const API = {
+  url: "/data/menu.json",
+  fetchMenu: async () => {
+    const response = await fetch(API.url);
+    return await response.json();
+  },
+};
+
+export default API;
