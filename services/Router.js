138aaf0cb446e7b4822933603a4a13214f08dc49
nkumar007 <naveen.mbuff98@gmail.com>
2023-08-21 18:40:21 +0530

    finishing setup of dynamic routes

--- services/Router.js
+++ services/Router.js
@@ -8,6 +8,11 @@ const Router = {
       });
     });
 
+    //Event listener for URL change
+    window.addEventListener("popstate", (e) => {
+      Router.go(e.state.route, false);
+    });
+
     // Check initial route
     Router.go(location.pathname);
   },
@@ -16,6 +21,34 @@ const Router = {
 
     if (addToHistory) {
       history.pushState({ route }, null, route);
+    }
+
+    let pageElement = null;
+
+    switch (route) {
+      case "/":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Menu";
+        break;
+      case "/order":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Order";
+        break;
+
+      default:
+        if (route.startsWith("/product-")) {
+          pageElement = document.createElement("h1");
+          pageElement.textContent = "Product";
+          const paramID = route.substring(route.lastIndexOf("-") + 1);
+          pageElement.dataset.id = paramID;
+        }
+    }
+
+    if (pageElement) {
+      document.querySelector("main").innerHTML = "";
+      document.querySelector("main").appendChild(pageElement);
+      window.screenX = 0;
+      window.screenY = 0;
     }
   },
 };
